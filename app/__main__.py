e937abcb6794839980c4045b33963580cfee8540
Giuseppe Santoro <giuseppe.santoro@gmail.com>
2023-04-29 16:07:45 +0100

    server a static html file from s3

--- app/__main__.py
+++ app/__main__.py
@@ -4,7 +4,31 @@ import pulumi
 from pulumi_aws import s3
 
 # Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+bucket = s3.Bucket('my-bucket',
+                   website=s3.BucketWebsiteArgs(index_document="index.html"))
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
+
+ownership_controls = s3.BucketOwnershipControls(
+    'ownership-controls',
+    bucket=bucket.id,
+    rule=s3.BucketOwnershipControlsRuleArgs(
+        object_ownership='ObjectWriter',
+    ),
+)
+
+public_access_block = s3.BucketPublicAccessBlock(
+    'public-access-block', bucket=bucket.id, block_public_acls=False
+)
+
+bucketObj = s3.BucketObject(
+    'index.html',
+    bucket=bucket.id,
+    source=pulumi.FileAsset("./data/index.html"),
+    content_type="text/html",
+    acl="public-read",
+    opts=pulumi.ResourceOptions(depends_on=[public_access_block])
+)
+
+pulumi.export('bucket_endpoint', pulumi.Output.concat('http://', bucket.website_endpoint))
